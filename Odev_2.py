8a02efee29dcd2606ce80372f034ab58622740c8
emrullah <eryilm4z@gmail.com>
2020-12-23 18:43:23 +0300

    Add files via upload

---
+++ Odev_2.py
@@ -0,0 +1,20 @@
+
+ad = input("Lütfen adınızı giriniz: ")
+soyad = input("Lütfen soyadınızı giriniz: ")
+yas= int(input("Lütfen yaşınızı giriniz: "))
+yıl= int(input("Lütfen hangi yılda doğduğunuzu yazınız: "))
+
+bilgiler = ad, soyad, yas, yıl
+
+a= list(bilgiler)
+print("-"*50)
+
+for i in a:
+  print(i)
+
+print("-"*50)
+
+if yas<18:
+  print("you can't go out because it's too dangerous")
+else:
+  print("you can go out the street")
